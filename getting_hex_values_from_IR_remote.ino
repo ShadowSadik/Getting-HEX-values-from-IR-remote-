cd52ebb2d72e994a36f170c07ca66414cafedab5
Md. Sadik Imtiaz Sadi <75381268+ShadowSadik@users.noreply.github.com>
2024-04-04 08:57:22 +0600

    Add files via upload

--- getting_hex_values_from_IR_remote.ino
+++ getting_hex_values_from_IR_remote.ino
@@ -3,6 +3,9 @@
 #include <IRutils.h>
 
 const uint16_t kRecvPin = D5;  // Pin connected to the IR receiver
+const int ledPin= D2;
+const int ledPin2= D1;
+
 
 IRrecv irrecv(kRecvPin);
 decode_results results;
@@ -10,6 +13,8 @@ decode_results results;
 void setup() {
   Serial.begin(115200);
   irrecv.enableIRIn();  // Start the IR receiver
+  pinMode (ledPin,OUTPUT);
+  pinMode (ledPin2,OUTPUT);
 }
 
 void loop() {
@@ -17,7 +22,26 @@ void loop() {
     // Print the hexadecimal representation of the received IR code
     Serial.print("Received 0x");
     Serial.println(results.value, HEX);
-    
+// turning on LED1
+    if (results.value == 0xFFA25DUL) 
+    { // Note the UL suffix for unsigned long
+    digitalWrite(ledPin, HIGH);
+    }
+    else if (results.value == 0xFF629DUL)
+    { // Note the UL suffix for unsigned long
+    digitalWrite(ledPin, LOW);
+    }
+// turning on LED2
+    if (results.value == 0xFFE21DUL) 
+    { // Note the UL suffix for unsigned long
+    digitalWrite(ledPin2, HIGH);
+    }
+    else if (results.value == 0xFF629DUL)
+    { // Note the UL suffix for unsigned long
+    digitalWrite(ledPin2, LOW);
+    }
+
+
     irrecv.resume();  // Receive the next IR code
   }
 }
